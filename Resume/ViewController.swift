c2af3c3c95b35684d5cca6a34466e336ad30b620
Novoselov Sergey <s.m.novoselov@icloud.com>
2019-08-02 22:08:32 +0300

    Initial Commit

---
+++ Resume/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Resume
+//
+//  Created by Sergey Novoselov on 02/08/2019.
+//  Copyright © 2019 Sergey Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
