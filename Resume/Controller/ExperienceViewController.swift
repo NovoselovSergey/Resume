2c3a8c8d5ea940392f7ac8f1485a8f6748c1cdaf
Novoselov Sergey <s.m.novoselov@icloud.com>
2019-08-05 00:06:57 +0300

    Create interface

---
+++ Resume/Controller/ExperienceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExperienceViewController.swift
+//  Resume
+//
+//  Created by Sergey Novoselov on 04/08/2019.
+//  Copyright © 2019 Sergey Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class ExperienceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Resume/Controller/SkillsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SkillsViewController.swift
+//  Resume
+//
+//  Created by Sergey Novoselov on 04/08/2019.
+//  Copyright © 2019 Sergey Novoselov. All rights reserved.
+//
+
+import UIKit
+
+class SkillsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
